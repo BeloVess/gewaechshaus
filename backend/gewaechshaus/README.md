05b47c31c152df36ef2e4938cb0567818ee269d9
Olga Pshenichnikova <ya.olgapshenichnikova@yandex.ru>
2020-12-08 22:23:23 +0300

    backend

---
+++ backend/gewaechshaus/README.md
@@ -0,0 +1,35 @@
+# Backend of Gewaechshaus project
+
+## Set DB
+
+For CentOS:  
+`grep 'temporary password' /var/log/mysqld.log`
+
+For all:  
+`sudo mysql_secure_installation`
+
+
+```
+create database gewaechshaus;
+GRANT ALL PRIVILEGES ON gewaechshaus.* TO 'gewaechshaus'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+```
+
+Reset password in CentOS:  
+
+```
+# systemctl stop mysqld
+# systemctl set-environment MYSQLD_OPTS="--skip-grant-tables"
+# systemctl start mysqld
+# mysql -u root
+mysql> UPDATE mysql.user SET authentication_string = PASSWORD('password') WHERE User = 'root' AND Host = 'localhost';
+mysql> FLUSH PRIVILEGES;
+mysql> quit
+# systemctl stop mysqld
+# systemctl unset-environment MYSQLD_OPTS
+# systemctl start mysqld
+```
+
+From Elementary OS:
+
+`sudo mysql -u root -p`
---
+++ backend/gewaechshaus/app/Http/Controllers/IndicationsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Indications;
+use Illuminate\Http\Request;
+
+class IndicationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Indications  $indications
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Indications $indications)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Indications  $indications
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Indications $indications)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Indications  $indications
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Indications $indications)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Indications  $indications
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Indications $indications)
+    {
+        //
+    }
+}
