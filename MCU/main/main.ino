77949280827f0dd66d75b9aa714c08e0723e7f49
Helga Weizenfeld <ya.olgapshenichnikova@yandex.ru>
2020-12-30 15:00:38 +0300

    Подключила датчик влажности

--- MCU/main/main.ino
+++ MCU/main/main.ino
@@ -1,15 +1,3 @@
-/*
-  Web client
- This sketch connects to a website (http://www.google.com)
- using an Arduino Wiznet Ethernet shield.
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe, based on work by Adrian McEwen
- */
-
 #include <SPI.h>
 #include <Ethernet3.h>
 
@@ -19,15 +7,23 @@ byte mac[] = { 0xDE, 0xAC, 0xBE, 0xEF, 0xFE, 0xED };
 char belowess[] = "belowess.ru";
 
 bool isConfigured = false;
+bool sendData = true;
+bool makeLight = false;
+
+// Light
 int relayPin = 8;
-bool sendData = false;
+// Moisture Meter
+int moisturePin = 4;
 
 void setup() {
   delay(2000);
    
   Serial.begin(9600);
   Serial.println("Setup");
-  pinMode(relayPin, OUTPUT);
+  
+  if (makeLight) {
+    pinMode(relayPin, OUTPUT);
+  }
 
   if (sendData) {
     Serial.println(Ethernet.linkReport());
@@ -49,74 +45,79 @@ void setup() {
 
 void loop()
 {
-  Serial.println("Loop");
+  Serial.println("Main");
+  
+  int moisture = analogRead(moisturePin);
+  //Serial.println(moisture);
+  
   if (sendData) {
     if (!isConfigured) {
       Serial.println("Not configured");
       while (true);
     }
     
-    while(true) {
-      long timestamp = 1609104050;
-      int value = 1;
+
+    long timestamp = 1609104050;
+    
+    String data = "{";
+    data.concat("\"measured_at\": ");
+    data.concat(String(timestamp));
+    data.concat(",");
+    data.concat("\"sensor_id\": 1,");
+    data.concat("\"isActive\": true,");
+    data.concat("\"value\": ");
+    data.concat(String(moisture));
+    data.concat("}");
+    Serial.println(data);
+    
+    if (client.connect(belowess, 8080)) {
+      Serial.println("connected");
       
-      String data = "{";
-      data.concat("\"measured_at\": ");
-      data.concat(String(timestamp));
-      data.concat(",");
-      data.concat("\"sensor_id\": 1,");
-      data.concat("\"isActive\": true,");
-      data.concat("\"value\": ");
-      data.concat(String(value));
-      data.concat("}");
-      Serial.println(data);
-      
-      if (client.connect(belowess, 8080)) {
-        Serial.println("connected");
-        
-        client.println("POST /api/records HTTP/1.1");
-        client.println("Host: belowess.ru:8080");
-        client.println("Content-Type: application/json");
-        client.println("Connection: keep-alive");
-        client.println("User-Agent: Arduino/1.0");
-        client.println("Accept: text/html,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-        client.print("Content-Length:");
-        client.println(data.length());
-        client.println();
-        client.print(data);
-  
-        client.flush();
-      }
-      else {
-        // kf you didn't get a connection to the server:
-        Serial.println("connection failed");
-        while (true);
-      }
-  
-      bool needFetch = true;
-      bool fetching = false;
-      
-      while(needFetch) {
-        if (client.available()) {
-          fetching = true;
-          char c = client.read();
-          Serial.print(c);
-        } else if (fetching) {
-          needFetch = false;
-          client.stop();
-          Serial.println("Fetched");
-        }
+      client.println("POST /api/records HTTP/1.1");
+      client.println("Host: belowess.ru:8080");
+      client.println("Content-Type: application/json");
+      client.println("Connection: close");
+      client.println("User-Agent: Arduino/1.0");
+      client.println("Accept: text/html,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+      client.print("Content-Length:");
+      client.println(data.length());
+      client.println();
+      client.print(data);
+
+      client.flush();
+    }
+    else {
+      Serial.println("connection failed");
+      while (true);
+    }
+
+    bool needFetch = true;
+    bool fetching = false;
+    
+    while(needFetch) {
+      if (client.available()) {
+        fetching = true;
+        char c = client.read();
+        Serial.print(c);
+      } else if (fetching) {
+        needFetch = false;
+        client.stop();
+        Serial.println("Fetched");
       }
     }
   }
 
-  Serial.println("Light");
-  while(true) {
-    delay(2000);
-    digitalWrite(relayPin,HIGH);
-    Serial.println("Light on");
-    delay(2000);
-    digitalWrite(relayPin,LOW);
-    Serial.println("Light off");
+  if (makeLight) {
+    Serial.println("Light");
+    while(true) {
+      delay(2000);
+      digitalWrite(relayPin,HIGH);
+      Serial.println("Light on");
+      delay(2000);
+      digitalWrite(relayPin,LOW);
+      Serial.println("Light off");
+    }
   }
+
+  delay(5000);
 }
